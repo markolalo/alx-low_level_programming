1516248f840b537c501a962c57c4264f1b69ac07
markolalo <markolalo@hotmail.com>
2022-03-13 22:50:30 -0700

    C file to print out string using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**C program to print out string value using puts function**/
+
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+	char str1[64];
+
+	strcpy(str1, "Programming is like building a multilingual puzzle");
+
+	puts(str1);
+
+	return(0);
+}
